be64debe1a4f237a572925defbf3d5ea39196359
Justin Gafford <jgaff001@ucr.edu>
2019-05-30 18:38:15 -0700

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "command.h"
+using namespace std;
+
+class Menu {
+    private:
+        int history_index; // Indexes which command was last executed, accounting for undo and redo functions
+        std::vector<Command*> history; // Holds all the commands that have been executed until now
+    public:
+        Menu() : history_index(0) {
+			
+        };
+        
+        void Menu::execute() {
+	          if (history.at(history_index) == NULL) {
+		            cout << 0 << endl;;
+		            return;
+	          }
+	          cout << history.at(history_index)->execute() << endl;
+        }
+
+        bool Menu::initialized() {
+	          if (history_index == 0) {
+		            return false;
+	          }
+	          return true;
+        }
+
+        void Menu::add_command(Command* cmd) {
+	          vector<Command*>::iterator it = history.begin();
+	          int counter = history_index + 1;
+
+	          if (history.empty()) {
+		            history.push_back(NULL);
+		            history.push_back(cmd);
+		            ++history_index;
+	          }
+	          else {
+		             if (history_index == history.size() - 1) {
+			               history.push_back(cmd);
+			               ++history_index;
+		             }
+		             else {
+			               while (counter != 0) {
+				                  ++it;
+				                  --counter;
+			               }
+			               history.at(history_index + 1) = cmd;
+			               ++history_index;
+		             }
+	          }
+       }
+
+       Command* Menu::get_command() {
+	         return history.at(history_index);
+       }
+
+       void Menu::undo() {
+	         if (history_index == 0) {
+		           return;
+	         }
+	         --history_index;
+	      }
+
+      void Menu::redo() {
+	        if (history_index == history.size() - 1) {
+		          return;
+	        }
+	        history_index++;
+      }
