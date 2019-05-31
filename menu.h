77c4cf960e535916ab4c3ca111a99624e217ec5b
vtsai006 <49217659+vtsai006@users.noreply.github.com>
2019-05-30 20:08:14 -0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -8,45 +8,47 @@ class Menu
     public:
         Menu() 
         {
-           
-            history_index = 0;
+            history_index = -1;
         }
         std::string execute() 
         {
-
+            if(history.size() == 0)
+                return;
+                
             return to_string(root->evaluate());
         }
         std::string stringify() 
         {
-           
             return to_string(root->stringify());
         }
+    
         bool initialized() 
         {
-
+            if(history.size() != 0)
+                return true;
             
             return false;
         }
+    
         void add_command(Command* cmd) 
         {
             history.push_back(cmd);
         }
         Command* get_command() 
         {
-          
             return history.at(history_index);
         }
+    
         void undo() 
         {
-           
             if(history_index != 0)
             {
                 return history.at(history_index-1);
             }
         }
+    
         void redo() 
         {
-
             if(history.size()>history_index)
             {
                 return history.at(history_index+1);
