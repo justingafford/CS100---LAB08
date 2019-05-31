08673d4f58841785f98019d7a6562a1d3f6f97f2
Justin Gafford <jgaff001@ucr.edu>
2019-05-30 18:51:44 -0700

    Update Div.cpp

--- Div.cpp
+++ Div.cpp
@@ -9,5 +9,6 @@ double Div::evaluate() {
 }
 
 string Div::stringify() {
-	return to_string(L1 ->evaluate()) + "/" + to_string(L2 ->evaluate());
+	string temp = L1 ->stringify() + "/" + L2 ->stringify();
+	return temp;
 }
